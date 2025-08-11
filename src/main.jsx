59397eafd879d9758874731c1907439697faf34b
nicole797 <nmail225@gmail.com>
2025-08-11 19:02:21 -0300

    importacion de bootstrap al proyecto

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterApp } from './router/RouterApp'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import "./index.css"
 import { UserProvider } from './context/UserContext'
