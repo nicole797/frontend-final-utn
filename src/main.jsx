388b0ba90a15495ea1337d172bf39f8537635751
nicole797 <nmail225@gmail.com>
2025-08-13 11:17:09 -0300

    importacion de js de bootstrap en el main

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterApp } from './router/RouterApp'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import "./index.css"
 import { UserProvider } from './context/UserContext'
 
